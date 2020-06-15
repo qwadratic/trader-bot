7eccde4594e4626ed1110152c017ba01d90a176f
Anton1912 <g.goreslavskyi19@gmail.com>
2020-06-16 00:49:27 +0300

    'spreadsheet_

--- jobs/ref.py
+++ jobs/ref.py
@@ -1,10 +1,8 @@
 from model import UserRef
 import datetime as dt
 
-
 def job_check_ref():
     (UserRef
      .delete()
      .where(dt.datetime.utcnow() - UserRef.ref_created_at > dt.timedelta(30))
      .execute())
-
