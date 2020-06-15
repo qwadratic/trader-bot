1c577fc6886e6a809a9bbabf964f97ad0ba3925f
Anton1912 <g.goreslavskyi19@gmail.com>
2020-06-16 00:52:57 +0300

    'model_modifier'

--- jobs/spreadsheet.py
+++ jobs/spreadsheet.py
@@ -7,5 +7,14 @@ def json_get(path='http://gsx2json.com/api?id=1ljtGgQoaLdU0HiYYAou4JlImbvYyBVHCK
     response = requests.get(path)
     data = json.loads(response.text)
     data_db = data['rows']
+    d = []
+    for i in data_db:
+        d.append(dict(
+            name=i['name'],
+            text=i['text'],
+            text_ru=i['textru'],
+            text_en=i['texten']
+        ))
     Text.delete().execute()
-    Text.insert(data_db).execute()
+    Text.insert(d).execute()
+
