44ab48925f93b32d73c460cb1e7dba1f0d03a2af
Anton1912 <59968195+Anton1912@users.noreply.github.com>
2020-06-22 13:13:07 +0300

    Update spreadsheet.py
    check_name_changes

--- jobs/spreadsheet.py
+++ jobs/spreadsheet.py
@@ -5,8 +5,8 @@ import requests
 # api_spreadsheets
 def json_get(path='http://gsx2json.com/api?id=1ljtGgQoaLdU0HiYYAou4JlImbvYyBVHCK2I6bEdqhiQ&sheet=1'):
     response = requests.get(path)
-    data = json.loads(response.text)
-    data_db = data['rows']
+    data_spreadsheet = json.loads(response.text)
+    data_db = data_spreadsheet['rows']
     d = []
     for i in data_db:
         d.append(dict(
@@ -15,6 +15,29 @@ def json_get(path='http://gsx2json.com/api?id=1ljtGgQoaLdU0HiYYAou4JlImbvYyBVHCK
             text_ru=i['textru'],
             text_en=i['texten']
         ))
-    Text.delete().execute()
-    Text.insert(d).execute()
+    return d
+
+def get_data_db():
+    query_text = Text.select().dicts()
+    data_text = []
+    for i in query_text:
+        data_text.append(dict(
+            name=i['name'],
+            text=i['text'],
+            text_ru=i['text_ru'],
+            text_en=i['text_en']
+        ))
+    return data_text
+
+def write_data():
+    new_data = json_get()
+    update_data = []
+    for i in new_data:
+        update_data.append(dict(
+            name=i['name'],
+            text=i['text'],
+            text_ru=i['text_ru'],
+            text_en=i['text_en']
+        ))
+    return Text.insert(update_data)
 
