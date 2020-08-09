5ba8a433aa684de2320321a5f82fe7b3999487e7
Anton1912 <g.goreslavskyi19@gmail.com>
2020-08-10 01:02:16 +0300

    'modifier_func'

--- bot/blockchain/rpc_btc.py
+++ bot/blockchain/rpc_btc.py
@@ -8,9 +8,9 @@ tx_fee = 0.00015
 rpc_connection = AuthServiceProxy("http://%s:%s@195.201.211.234:8878%s"%
                                   (rpc_user, rpc_password, uri_path_wallet))
 
-def create_new_wallet (wallet_name): #TODO to define wallet_name
+def create_new_wallet (wallet_name):
     new_wallet = rpc_connection.createwallet(wallet_name)
-    return print(new_wallet)
+    return new_wallet
 
 def get_wallet_balance():
     wallet_balance = rpc_connection.getbalance()
@@ -40,7 +40,7 @@ def get_new_adress(wallet): #TODO logic to identify
     return new_adr
 
 def create_transaction():
-    received_address = ''
+    received_address = '' #TODO define received_address
     sum_btc = None # integer
     new_transaction = None
 
