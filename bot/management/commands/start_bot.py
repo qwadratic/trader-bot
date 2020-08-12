32de40f993cb6ca653cb7b16ee5c100726e24714
Anton1912 <g.goreslavskyi19@gmail.com>
2020-08-12 14:10:50 +0300

    'add_check_refill_btc'

--- bot/management/commands/start_bot.py
+++ bot/management/commands/start_bot.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 from pyrogram import Client
 from apscheduler.schedulers.background import BackgroundScheduler
 
-from bot.jobs import check_refill_bip, check_refill_eth, update_exchange_rates, get_update_exchange_rates_interval
+from bot.jobs import check_refill_bip, check_refill_eth, update_exchange_rates, get_update_exchange_rates_interval, check_refill_btc, update_wallet_balance
 
 from config.settings import TG_API_ID, TG_API_HASH, TG_API_TOKEN
 
@@ -20,6 +20,8 @@ class Command(BaseCommand):
         shed.add_job(check_refill_bip, 'interval', seconds=5, args=[app])
         shed.add_job(check_refill_eth, 'interval', seconds=20, args=[app])
         shed.add_job(update_exchange_rates, 'interval', minutes=get_update_exchange_rates_interval())
+        shed.add_job(check_refill_btc, 'interval', seconds=5, args=[app])
+        #shed.add_job(update_wallet_balance, 'interval', seconds=5, args=[app])
 
         shed.start()
 
