bc3e5c228fbac8737b3c02c605cf83c3e5bc0d64
Anton1912 <g.goreslavskyi19@gmail.com>
2020-08-14 21:12:40 +0300

    n

--- bot/jobs/btc.py
+++ bot/jobs/btc.py
@@ -19,14 +19,17 @@ def check_refill_btc(cli):
         service.save()
 
 def update_wallet_balance():
-    user_wallets = VirtualWallet.objects.get(currency='BTC')
+    user_wallets = VirtualWallet.objects.filter(currency='BTC')
     user_name = []
     user_name.append(user_wallets.user_id)
+
     for name in user_name:
         update_balance = []
         wallet_name = name
         actual_balance = get_wallet_balance(wallet_name)
         update_balance.append(actual_balance)
+
+
         # костыль
         try:
             VirtualWallet.objects.update(balance=update_balance)
