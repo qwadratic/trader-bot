cd64d66dcf1d75453dd160ec3af03cbfede7cfc9
Anton1912 <g.goreslavskyi19@gmail.com>
2020-08-30 13:26:57 +0300

    change DEBUG=False

--- config/settings/django.py
+++ config/settings/django.py
@@ -21,7 +21,7 @@ from .env import env
 SECRET_KEY = env.str('SECRET_KEY', default='qwerty')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG', default=False)
+DEBUG = env.bool('DEBUG', default=False) # necesary to change in .env DEBUG=off
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
