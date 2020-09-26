4957defc9d677a7cb66b2fdce78769aba49c1a79
cyace84 <cyace84@gmail.com>
2020-09-26 19:55:34 +0300

    todo get_currency_rate

--- bot/helpers/shortcut.py
+++ bot/helpers/shortcut.py
@@ -114,7 +114,7 @@ def get_currency_rate(currency):
     if currency == 'USD':
         return to_cents('USD', 1)
 
-    return ExchangeRate.objects.filter(currency=currency, source='coinmarketcup').latest('time').value
+    return ExchangeRate.objects.filter(currency=currency, source='coinmarketcup').latest('time').value  # TODO: Пофиксить на разные источники
 
 
 def create_record_cashflow(user, to, type_operation, amount, currency, trade=None, tx_hash=None):
