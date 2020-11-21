58d7c7f3fb0bbdcc444e24c58e0526b80a35ae92
Anton1912 <g.goreslavskyi19@gmail.com>
2020-11-22 00:48:04 +0200

    fix TB-2/button START

--- bot/handlers/registration.py
+++ bot/handlers/registration.py
@@ -81,11 +81,12 @@ def ref_start(_, m):
     m.reply(user.get_text(name=text_name), reply_markup=markup)
 
 
-@Client.on_message(Filters.text & ~filter_kb, group=-1)
+@Client.on_message(Filters.text | ~filter_kb, group=-1)
 def start_m(_, m):
     tg_user = m.from_user
     user = get_user(tg_user.id)
-    if not user and m != '/start':
+
+    if not user and m.text != '/start':
         m.reply('Для начала работы с ботом - нажмите START', reply_markup=kb.start())
 
 
