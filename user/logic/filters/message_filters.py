05c23c831ce830da9c299de1ff33ceefe9451f83
Anton1912 <g.goreslavskyi19@gmail.com>
2020-11-22 11:34:38 +0200

    removed unnecessary filters

--- user/logic/filters/message_filters.py
+++ user/logic/filters/message_filters.py
@@ -1,8 +1,6 @@
 from pyrogram import Filters
-from user.models.user import Text
+from user.models.user import Text, TelegramUser
 
 ref_link = Filters.create(lambda _, m: len(m.command) > 1)
-
-filter_kb = Filters.create(lambda _, m: m.text == Text.objects.get(text_ru=m.text))  #TODO не учтена мультиязычность
 
 filter_admin = Filters.user([69062067, 373283223, 862797627, 263425422])
